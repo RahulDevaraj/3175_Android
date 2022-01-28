62515616b6b5556b28b01aaaec2e51686806e05c
“Rahul <kannan.vegeta@gmail.com>
2022-01-27 21:29:52 -0800

    Lec3Demo Completed

--- Lec2Demo/app/src/main/java/com/example/lec2demo/MainActivity.java
+++ Lec2Demo/app/src/main/java/com/example/lec2demo/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -31,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         editTextNumTxt = findViewById(R.id.editTextNumTxt);
 
         spinnerConcertType = findViewById(R.id.spinnerConcertType);
+
+        spinnerConcertType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Corresponding index on spinner
+                if(i==0)
+                    Toast.makeText(MainActivity.this, "Rock Band", Toast.LENGTH_SHORT).show();
+                else if(i==1)
+                    Toast.makeText(MainActivity.this, "Jazz Band", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Blues Band", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         btnCalcCost.setOnClickListener((View view) -> {
     if(editTextNumTxt.getText().toString().isEmpty()){
--- Lec3Demo/app/src/main/java/com/example/lec3demo/MainActivity.java
+++ Lec3Demo/app/src/main/java/com/example/lec3demo/MainActivity.java
@@ -4,8 +4,19 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    TextView textViewResults;
+    EditText editTextInputWeight;
+    Button buttonConvertWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +29,61 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setLogo(R.mipmap.ic_launcher_wt_round);
 
         actionBar.setTitle(R.string.txtTitle);
+
+        radioGroup = findViewById(R.id.radGropConvType);
+        editTextInputWeight = findViewById(R.id.editTextInputWeight);
+        buttonConvertWeight = findViewById(R.id.btnConvertWt);
+        textViewResults = findViewById(R.id.textViewResults);
+
+        buttonConvertWeight.setOnClickListener((View view)-> {
+            if(radioGroup.getCheckedRadioButtonId() == -1)
+            {
+                Toast.makeText(MainActivity.this, "Please Select an Option", Toast.LENGTH_SHORT).show();
+            }
+            else if(editTextInputWeight.getText().toString().isEmpty())
+            {
+                Toast.makeText(this, "Please ENter baggage weight", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                double inputWt,outputWt=0;
+                try{
+                    inputWt = Double.parseDouble(editTextInputWeight.getText().toString());
+
+                    if(inputWt <0)
+                        Toast.makeText(MainActivity.this, "Weight cannot be -ve", Toast.LENGTH_SHORT).show();
+                    else if(radioGroup.getCheckedRadioButtonId() == R.id.radBtnKgsToLbs)
+                    {
+                        if(inputWt > 500)
+                            Toast.makeText(this, "Input Wight cannot be more than 500 Kgs",
+                                    Toast.LENGTH_SHORT).show();
+                        else
+                        {
+                            outputWt = inputWt * 2.2;
+                            textViewResults.setText(String.format("Converted wt: %.2f Lbs",outputWt));
+                        }
+                    }
+                    else if(radioGroup.getCheckedRadioButtonId() == R.id.radBtnLbsToKgs)
+                    {
+                        if(inputWt > 1000)
+                            Toast.makeText(this, "Input Wight cannot be more than 1000Lbs",
+                                    Toast.LENGTH_SHORT).show();
+                        else
+                        {
+                            outputWt = inputWt / 2.2;
+                            textViewResults.setText(String.format("Converted wt: %.2f Kgs",outputWt));
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        });
+
+
+
     }
 }
