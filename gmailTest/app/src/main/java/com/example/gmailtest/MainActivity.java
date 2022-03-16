226f57f15f8cd7dabdba7b38800331abf9658e8a
RahulDevaraj <kannan.vegeta@gmail.com>
2022-03-15 21:05:56 -0700

    Email test added

---
+++ gmailTest/app/src/main/java/com/example/gmailtest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.gmailtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import javax.mail.MessagingException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonSendMail);
+
+        button.setOnClickListener((View view)-> {
+
+            try {
+                sendMail();
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+    }
+
+    private void sendMail() throws MessagingException {
+
+        String senderAddress = "insights041@gmail.com";
+        String subject = "Test Subject";
+        String message = "Test message";
+
+
+        MailHelper mailHelper = new MailHelper(this, senderAddress, subject, message);
+
+        mailHelper.execute();
+    }
+}
