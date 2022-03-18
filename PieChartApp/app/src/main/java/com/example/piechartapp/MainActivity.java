60e80592e8def7c6d3a48030d3225b2c31bec5b5
RahulDevaraj <kannan.vegeta@gmail.com>
2022-03-17 23:36:58 -0700

    Pie Chart

---
+++ PieChartApp/app/src/main/java/com/example/piechartapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.piechartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private PieChart pieChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pieChart = findViewById(R.id.pieChartWidget);
+
+        ArrayList<PieEntry> entries = addPieData();
+        if(entries.size()<1){
+
+        }
+        else
+        {
+            setupPieChart("here");
+            loadPieChartData(entries);
+        }
+    }
+
+    private void setupPieChart(String centerText) {
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterText(centerText);
+        pieChart.setCenterTextSize(18);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setWordWrapEnabled(true);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+    private void loadPieChartData(ArrayList<PieEntry> arrayList) {
+        ArrayList<PieEntry> entries = new ArrayList<>(arrayList);
+
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+        pieChart.animateY(1400, Easing.EaseInOutQuad);
+    }
+
+    private ArrayList<PieEntry> addPieData(){
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(15, "Food & Dining"));
+        entries.add(new PieEntry(30, "Medical"));
+        entries.add(new PieEntry(40, "Entertainment"));
+        entries.add(new PieEntry(100, "Electricity and Gas"));
+        entries.add(new PieEntry(125, "Housing"));
+        return entries;
+    }
+
+}
